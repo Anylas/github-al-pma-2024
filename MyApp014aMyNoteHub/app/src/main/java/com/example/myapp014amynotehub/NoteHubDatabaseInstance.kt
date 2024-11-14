c3ec4f444a2a3c9c0089d2ce5f0f264089f77735
AnetaLastovickova <aneta.lastovickova@tul.cz>
2024-11-14 12:06:31 +0100

    #14 MOD MyApp014aMyNoteHub first editing

---
+++ MyApp014aMyNoteHub/app/src/main/java/com/example/myapp014amynotehub/NoteHubDatabaseInstance.kt
@@ -0,0 +1,25 @@
+package com.example.myapp014amynotehub
+import android.content.Context
+import androidx.room.Room
+
+object NoteHubDatabaseInstance {
+
+    // Lazy inicializace instance databáze
+    @Volatile //ajistí, že se hodnota INSTANCE okamžitě projeví všem vláknům, což je důležité pro bezpečný přístup k jediné instanci.
+    private var INSTANCE: NoteHubDatabase? = null
+
+    // Funkce pro získání instance databáze
+    fun getDatabase(context: Context): NoteHubDatabase {
+        // Pokud je instance null, inicializuje ji
+        return INSTANCE ?: synchronized(this) {
+            val instance = Room.databaseBuilder(
+                context.applicationContext,
+                NoteHubDatabase::class.java,
+                "notehub_database"
+            ).build()
+            INSTANCE = instance
+            instance
+        }
+    }
+
+}
